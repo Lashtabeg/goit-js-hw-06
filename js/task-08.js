acf877f39ec7215aa3c59362228b2c064884a0a0
Lashtabeg <tahcik@live.com>
2023-07-23 13:25:01 +0800

    Done!

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener(`submit`,onFormSub);
+
+function onFormSub(event) {
+    event.preventDefault();
+        // console.log( event.currentTarget.elements)
+    const formElms= event.currentTarget.elements;
+if (formElms.email.value===``||formElms.password.value === ``) {
+    return alert(`Всі поля повинні бути заповнені! (-^-^-)` )
+   }  
+//    else{const formData = new FormData(event.currentTarget)
+//     console.log(formData)    }
+else{
+    const dataForm = {
+        email: formElms.email.value,
+        password:formElms.password.value,
+    }
+    console.log( dataForm);   
+}
+event.currentTarget.reset();
+}
